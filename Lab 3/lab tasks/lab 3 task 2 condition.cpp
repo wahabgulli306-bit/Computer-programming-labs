73f9f5463bce8d27f36d28d5cc4855c5b612a556
wahabgulli306-bit <wahabgulli306@gmail.com>
2026-03-08 23:29:29 +0500

    Add files via upload

---
+++ Lab 3/lab tasks/lab 3 task 2 condition.cpp
@@ -0,0 +1,21 @@
+/* Name:Abdul Wahab*/
+/* Roll No: 5126053 / BF25NWELE0704 */
+                             
+#include <iostream>             // tells the compiler to include standard input output library
+using namespace std;               // allows us to use cout without having to type std:: for each
+
+int main(){                    // the starting point from where the computer begins executing program
+	
+	int a;                      // variable declaration
+	
+	cout<<"enter an integer ";     // prompt for user to input data
+	cin>>a;                           // takes data from user and puts it into the variable
+	
+	if(a>0 && a<100)                    // condition for the program to cheak a number is greater than 0 and less than 100
+	cout<<"condition met between 0 and 100"<<endl;  // if the condition met the message is sent
+	
+	else                                              // part that runs if the if condition fails
+	cout<<"condition not met"<<endl;         // if the condion doesnt met this message is sent 
+	
+	return 0;   // tells the operating system that program has finished
+}
---
+++ Lab 3/lab tasks/lab 3 task 5 else if.cpp
@@ -0,0 +1,22 @@
+/* Name:Abdul Wahab*/
+/* Roll No: 5126053 / BF25NWELE0704 */
+
+#include <iostream>  // tells the compiler to include standard input output stream library
+using namespace std;  // allows us to use cout and cin without having the need to typ std:: for each cin and cout
+
+int main() {                 // main function execution beggins from here 
+	
+	int a;                                // variable declarartion
+	cout<<"enter a number";             // displays are request to the user
+	cin>>a;                                     // stores the user input data into variable 
+	if(a>0)                                   // cheaks if the number is greater than 0
+	cout<<"the number is positive";               // displays thre message if the number is greater than 0
+	else if(a<0)                                // if test 1 fails , it cheaks if the number is less than o
+	cout<<"the number is negative";              // confirms that the number is negative
+	else if(a==0)                              // cheaks that if the number is exactly 0
+	cout<<"the number you entered is zero";         // final confirmation for 0
+	
+	return 0;                            // tells the operating system that program has finishe
+	
+
+}
