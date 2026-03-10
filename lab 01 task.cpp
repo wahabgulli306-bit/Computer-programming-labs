c74d4961dc33b175d51302fffa1bf0e0a5158b15
wahabgulli306-bit <wahabgulli306@gmail.com>
2026-03-10 10:21:29 +0500

    Add files via upload

---
+++ lab 01 task.cpp
@@ -0,0 +1,13 @@
+            // NAME =Abdul wahab
+             // ID = 0704
+
+#include <iostream>            // tells the compiler to include input output stream library 
+using namespace std;        // allows us to use standard namespace cout and c in without have the need to write std:: for each
+
+int main() {                  // main function from where the execution begins
+	
+	cout <<"Hello world !"<<endl; // output to the display 
+	
+	return 0;          // tells that program has ended successfully
+
+}                        
