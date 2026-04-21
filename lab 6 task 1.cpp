33803c72d21ebcefc9882ef35fc4c5bfa5fbff7f
wahabgulli306-bit <wahabgulli306@gmail.com>
2026-04-21 20:15:46 +0500

    Add files via upload

---
+++ lab 6 task 1.cpp
@@ -0,0 +1,15 @@
+/*Abdul Wahab
+  Reg No : BF25NWELE0704 */
+
+#include <iostream> // library for input and output
+using namespace std; // to use cout without std::
+
+int main() { // main function starts
+
+    // for loop: initialization, condition, increment
+    for (int i = 1; i <= 10; i++) { // start from 1, run till 10, increase by 1
+        cout << i << endl; // print number on separate line
+    }
+
+    return 0; // program ends successfully
+}
