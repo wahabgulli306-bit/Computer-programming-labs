74bfc949926a12b35db4950e2b35b6c3a7228918
wahabgulli306-bit <wahabgulli306@gmail.com>
2026-05-18 21:36:32 +0500

    Add files via upload

---
+++ Lab 11/Lab 11 task 1.cpp
@@ -0,0 +1,18 @@
+/*Abdul Wahab
+BF25NWELE0704*/
+
+#include <iostream>   // Library for input and output
+#include <string>     // Library for string functions
+using namespace std;
+
+int main()
+{
+    string message = "Hello Wahab"; // Declare and initialize string
+
+    int length = message.length();  // Find length of string
+
+    cout << "String = " << message << endl; // Display string
+    cout << "Length = " << length << endl;  // Display length
+
+    return 0; // End program
+}
---
+++ Lab 11/Lab 11 task 2.cpp
@@ -0,0 +1,18 @@
+/*Abdul Wahab
+BF25NWELE0704*/
+
+#include <iostream>   // Library for input and output
+#include <string>     // Library for string functions
+using namespace std;
+
+int main()
+{
+    string firstName = "Abdul"; // First name
+    string lastName = "Wahab";  // Last name
+
+    string fullName = firstName + " " + lastName; // Join strings
+
+    cout << "Full Name = " << fullName << endl; // Display full name
+
+    return 0; // End program
+}
